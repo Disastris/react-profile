cac4e9ec2252e73ba7fa4426b22619e975d8fcfd
Disastris <chavez.garcia.santi@gmail.com>
2023-11-26 16:46:57 -0600

    Installed Tailwind and started on the one page profile

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import About from "./pages/About.jsx";
+import Project from "./pages/Project.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <About />, // localhost:3001
+      },
+      {
+        path: "/Project", // localhost:3001/Project
+        element: <Project />,
+      },
+      // {
+      //   path: "/Resume",
+      //   element: <Resume />,
+      // },
+      // {
+      //   path: "/Contact",
+      //   element: <Contact />,
+      // },
+    ],
+  },
+]);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
