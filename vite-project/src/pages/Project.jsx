d229ba83b672e342c6ac93456df606015e2d5c1e
Disastris <chavez.garcia.santi@gmail.com>
2023-11-28 23:27:17 -0600

    Got started on the Project Page

--- vite-project/src/pages/Project.jsx
+++ vite-project/src/pages/Project.jsx
@@ -1,9 +1,43 @@
 function Project() {
+  const Project = [
+    {
+      title: "Cinephile",
+      description:
+        "An application made to help people find movie related articles and learn more about certain movies",
+      live: "https://chaseostien.github.io/Cinephile/",
+      code: "https://github.com/ChaseOstien/Cinephile",
+    },
+    {
+      title: "UT Bro's Apparel",
+      description:
+        "An inventory management application made to add, delete, and update inventory for convenience",
+      live: "https://ut-bros-apparel-0bcd7da3cb4b.herokuapp.com/login",
+      code: "https://github.com/cprademacher/apparel-database",
+    },
+    {
+      title: "Auctionator",
+      description:
+        "A fun application made in the style of Deal Dash with less noise in the background for easier purchase",
+      live: "https://auctionator-12c2490092de.herokuapp.com/#",
+      code: "https://github.com/Villzies/Auctionator",
+    },
+  ];
   return (
-    <>
-      <div>Project</div>
-    </>
+    <section className="bg-primary text-black px-5 py-32" id="projects">
+      <div className="container mx-auto grid md:grid-cols-2 items-center md:justify-between">
+        <div className="about-info mb-5">
+          <h2 className="text-4xl font-bold mb-5 border-b-[5px] w-[180px] border-yellow-200 pb-2">
+            Projects
+          </h2>
+
+          <p className="pb-5">
+            These are some of my best projects. The respective languages used
+            for each project will be found in their respective Github
+            repositories. Go ahead and give them a click!
+          </p>
+        </div>
+      </div>
+    </section>
   );
 }
-
 export default Project;
