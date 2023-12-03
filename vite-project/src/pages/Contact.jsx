aedfac7d1e8ceca034411ac8df58d57c3dc01bb0
Disastris <chavez.garcia.santi@gmail.com>
2023-12-02 22:16:10 -0600

    Created Contact From without styling

--- vite-project/src/pages/Contact.jsx
+++ vite-project/src/pages/Contact.jsx
@@ -1,26 +1,31 @@
+import ContactForm from "../components/contactForm";
+
 function Contact() {
   return (
-    <section className="bg-secondery px-5 py-32" id="contact">
-      <div className="text-center md:w-[60%] mx-auto">
-        <div className="text-4xl font-bold mb-5 border-b-[5px] w-[200px] mx-auto border-yellow-200 pb-2">
-          Contact
+    <div>
+      <ContactForm />
+      <section className="bg-secondery px-5 py-32" id="contact">
+        <div className="text-center md:w-[60%] mx-auto">
+          <div className="text-4xl font-bold mb-5 border-b-[5px] w-[200px] mx-auto border-yellow-200 pb-2">
+            Contact
+          </div>
+          <p>
+            I am open to work any position, either front or backend coding. I am
+            certified to work either positions and would love to hear from you.
+            You can contact me easily thru my email and i also provided my
+            Github so you can look at some other work.
+          </p>
+          <p className="py-2">
+            <span className="font-bold">Email:</span>{" "}
+            Chavez.Garcia.Santi@gmail.com
+          </p>
+          <p className="py-2">
+            <span className="font-bold">Github:</span>{" "}
+            https://github.com/Disastris
+          </p>
         </div>
-        <p>
-          I am open to work any position, either front or backend coding. I am
-          certified to work either positions and would love to hear from you.
-          You can contact me easily thru my email and i also provided my Github
-          so you can look at some other work.
-        </p>
-        <p className="py-2">
-          <span className="font-bold">Email:</span>{" "}
-          Chavez.Garcia.Santi@gmail.com
-        </p>
-        <p className="py-2">
-          <span className="font-bold">Github:</span>{" "}
-          https://github.com/Disastris
-        </p>
-      </div>
-    </section>
+      </section>
+    </div>
   );
 }
 
